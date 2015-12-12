149608f92ad458a74003a9627ed4623bb3d84c24
J. M. Mubasshir Rahman <iammubasshir@gmail.com>
2015-12-12 23:44:51 +0600

    Add Data Validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,17 @@ class Restaurant < ActiveRecord::Base
 
 	  has_many :reviews
 
+	  validates :name, :address, :phone, :website, :image, presence: true
+	  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    	message: "Must be in the format {123} 456-7890" }
+	 
+	  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    	message: "Must start with http:// or https://" }
+	 validates :address, format: { with: /\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    	message: "Must be in format 36 New Yourk, Banorgati, KL 91000" }
+
+    	
+
+
+
 end
